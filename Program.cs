f43a229dcdd636c42731b2c8f133cc87f18a616b
Patrickc132 <70346571+Patrickc132@users.noreply.github.com>
2020-09-24 21:13:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace name_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            
+            Console.Write("Please enter your full name");
+            Console.WriteLine("");
+            //this instructs the user
+
+            try
+            {
+                string input = Console.ReadLine();
+
+                //this will run the method
+                Name(input);
+            }
+            catch
+            {
+                //error catch
+                Console.WriteLine("An error has occured try again");
+                Console.WriteLine("Press any key to try again!");
+                Console.ReadKey(true);
+            }
+            
+
+            static void Name(string input)
+            {
+                //this method will output the users name
+
+                Console.WriteLine("Hello " + input.ToString() + ".");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+                
+
+            }                     
+        }
+    }
+}
